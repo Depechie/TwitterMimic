cea37d9dff2052f77e3cbae0c01e095cb2866a41
Depechie <glenn.versweyveld@gmail.com>
2013-08-30 00:11:32 +0200

    Initial commit

---
+++ TwitterMimic/Controls/PathControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TwitterMimic.Controls
+{
+    public class PathControl : ContentControl
+    {
+        public PathControl()
+        {
+            DefaultStyleKey = typeof(PathControl);
+        }
+    }
+}
---
+++ TwitterMimic/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.Phone.Controls;
+
+namespace TwitterMimic
+{
+    public class PathData
+    {
+        public string XAMLContent { get; set; }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        private void ImageBar_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            this.ContentPivot.SelectedIndex = this.ImageBar.SelectedIndex;
+        }
+
+        private void ContentPivot_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            this.ImageBar.SelectedIndex = ((Pivot)sender).SelectedIndex;
+        }
+    }
+}
